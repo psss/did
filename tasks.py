2e2ada564953f294e84c6e03e00acac302fa20e4
Chris Ward <cward@redhat.com>
2015-09-07 13:39:26 +0200

    log fallback; test; docs; clean_git

--- tasks.py
+++ tasks.py
@@ -6,7 +6,14 @@ from __future__ import absolute_import, unicode_literals
 
 from invoke import task, run
 
-from did.utils import log, LOG_INFO
+try:
+    from did.utils import Logging, LOG_INFO
+    _logging = Logging('did.tasks')
+    log = _logging.logger
+except Exception as e:
+    import logging
+    LOG_INFO = logging.INFO
+    log = logging.getLogger('did.tasks')
 
 # set log level to INFO
 log.setLevel(LOG_INFO)
@@ -14,8 +21,61 @@ log.setLevel(LOG_INFO)
 
 @task
 def build(sdist=False, rpm=False):
+    '''
+    Build
+    -----
+
+    '''
     log.info("Running build command!")
     if sdist:
         run("python setup.py sdist")
     if rpm:
         run("make rpm")
+
+
+@task
+def test(coverage=False):
+    '''
+    Test
+    ----
+
+    '''
+    log.info("Running Test command!")
+    if coverage:
+        log.info(" ... Running Coverage!")
+        run("coverage run --source=did -m py.test tests")
+    else:
+        run("py.test tests/")
+
+
+@task
+def docs(html=False):
+    '''
+    Test
+    ----
+
+    '''
+    log.info("Running Documentation command!")
+    _cmd = "cd docs;"
+    if html:
+        cmd = "{0} make html".format(_cmd)
+    else:
+        cmd = "{0} make html".format(_cmd)
+    run(cmd)
+
+
+@task
+def clean_git(force=False, options='Xd'):
+    '''
+    Test
+    ----
+
+    '''
+    log.info("Running Git Repository Clean command")
+    force = 'f' if force else 'n'
+    cmd = "git clean"
+    if options:
+        _options = ' -{}{}'.format(options, force)
+        cmd += _options
+    log.info(" ... Options: {}".format(_options))
+    run(cmd)
