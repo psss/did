8a725af50c55da28d4f164704871cdc50366bc4a
Petr Šplíchal <psplicha@redhat.com>
2015-09-09 21:17:17 +0200

    Simplify git plugin tests

--- tests/plugins/test_git.py
+++ tests/plugins/test_git.py
@@ -1,88 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Author: "Chris Ward" <cward@redhat.com>
+# coding: utf-8
+""" Tests for the git plugin """
 
 from __future__ import unicode_literals, absolute_import
 
 import os
-from did import utils
+import did.did
+import did.utils
 
 
-class Mock(object):
-    pass
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  Constants
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-script_path = os.path.dirname(os.path.realpath(__file__))
-# go all the way to the git repo root (2 levels up)
-git_path = os.path.realpath('{0}/../../'.format(script_path))
-
+# Prepare interval and config file with real path to our git repo
+INTERVAL = "--since 2015-09-07 --until 2015-09-11"
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+GIT_PATH = os.path.realpath('{0}/../../'.format(SCRIPT_PATH))
 CONFIG = """
 [general]
-email = "Chris Ward" <cward@redhat.com>
-[test_header]
+email = "Petr Splichal" <psplicha@redhat.com>
+
+[tools]
 type = git
 did = {0}
-""".format(git_path)
-
-options = Mock()
-options.since = '2015-04-01'
-options.until = '2015-04-24'
-options.verbose = True
-
-user_cward = Mock()
-user_cward.login = 'cward'
-user_psplicha = Mock()
-user_psplicha.login = 'psplicha'
-
-parent = Mock()
-parent.options = options
-parent.user = user_cward
+"""
 
 
-def test_GitRepo():
-    utils.Config(CONFIG)
-    from did.plugins.git import GitRepo
-    assert GitRepo
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  Tests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    repo = GitRepo(path=git_path)
-    commits = repo.commits(user=user_cward, options=options)
-    assert len(commits) > 0
+def test_git_regular():
+    """ Simple git stats """
+    did.utils.Config(CONFIG.format(GIT_PATH))
+    did.did.main(INTERVAL)
 
-    repo = GitRepo(path='bad path')
+def test_git_verbose():
+    """ Verbose git stats """
+    did.utils.Config(CONFIG.format(GIT_PATH))
+    did.did.main(INTERVAL + " --verbose")
+
+def test_git_invalid():
+    """ Invalid git repo """
+    did.utils.Config(CONFIG.format("i-do-not-exist"))
     try:
-        repo.commits(user=user_cward, options=options)
-    except utils.ReportError:
+        did.did.main(INTERVAL)
+    except SystemExit:
         pass
     else:
-        raise RuntimeError("expected a failure")
-
-
-def test_GitCommits():
-    utils.Config(CONFIG)
-    from did.plugins.git import GitCommits
-    assert GitCommits
-
-    stats = GitCommits(option='test_header',
-                       name='Working on topic',
-                       parent=parent,
-                       path=git_path)
-    stats.user = user_cward
-    stats.fetch()
-    assert len(stats.stats) > 0
-
-    stats.user = user_psplicha
-    stats.fetch()
-    assert len(stats.stats) > 0
-
-
-def test_GitStats():
-    utils.Config(CONFIG)
-    from did.plugins.git import GitStats
-    assert GitStats
-
-    stats = GitStats(option='test_header',
-                     name='Working on topic',
-                     parent=parent)
-
-    stats.user = user_cward
-    stats.fetch()
+        raise RuntimeError("Expected failure")
