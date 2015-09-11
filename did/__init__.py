72aaa5d1840a32b1c67f4d07c3d85f1e752dbc4a
Petr Šplíchal <psplicha@redhat.com>
2015-09-11 10:28:33 +0200

    Move module description to the module itself
    In this way it will be nicely shared for both online documentation
    and also directly available through "pydoc did".

--- did/__init__.py
+++ did/__init__.py
@@ -1,7 +1,7 @@
 """
-What did you do last week, month, year?
-
-Comfortably gather status report data (e.g. list of committed
-changes) for given week, month, quarter, year or selected date
-range. By default all available stats for this week are reported.
+The `stats`_ module contains the core of the stats gathering
+functionality. Some basic functionality like exceptions, config,
+user and date handling is placed in the `base`_ module. Generic
+utilities can be found in the `utils`_ module. Option parsing
+and other command line stuff resides in the `cli`_ module.
 """
--- docs/modules.rst
+++ docs/modules.rst
@@ -3,11 +3,9 @@
     Modules
 ===============
 
-The `stats`_ module contains the core of the stats gathering
-functionality. Some basic functionality like exceptions, config,
-user and date handling is placed in the `base`_ module. Generic
-utilities can be found in the `utils`_ module. Option parsing
-and other command line stuff resides in the `cli`_ module.
+.. automodule:: did
+    :members:
+    :undoc-members:
 
 stats
 -----
