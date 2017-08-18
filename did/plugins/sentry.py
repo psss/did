9f28b73fc4e3bef95160a1ebeffb6a523d02b4f7
Maros Kopec <makopec@redhat.com>
2017-08-18 13:54:01 +0200

    Added initial file for sentry plugin

---
+++ did/plugins/sentry.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+"""
+Sentry stats such as commented, resolved issues
+
+Configuration example::
+
+    [sentry]
+    type = sentry
+    url = http://sentry.usersys.redhat.com/api/0/
+    token = a1s2d3f4a1s2d3f4a1s2d3f4a1s2d3f4a1s2d3f4
+
+You need to generate authentication token
+at http://sentry.usersys.redhat.com/api/.
+The only scope you need to check is `org:read`.
+"""
+
