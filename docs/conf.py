08c92771206b81a3921676241dda378530d1e554
Petr Šplíchal <psplicha@redhat.com>
2019-02-04 17:12:58 +0100

    Mock bugzilla module to fix generating docs

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
 	return Mock()
 
-MOCK_MODULES = ['gssapi', 'requests_gssapi', 'bitly_api']
+MOCK_MODULES = ['gssapi', 'requests_gssapi', 'bitly_api', 'bugzilla']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
