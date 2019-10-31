94323c60f44083bdd94b04ed10b42e56ee56e230
Miroslav Vadkerti <thrix@vadkerti.net>
2019-10-31 12:32:07 +0100

    Simple localhost provisioner (#28)
    Is a very simple provisioner for running tests on localhost.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- tmt/steps/provision/localhost.py
+++ tmt/steps/provision/localhost.py
@@ -4,8 +4,5 @@ from tmt.steps.provision.base import ProvisionBase
 
 
 class ProvisionLocalhost(ProvisionBase):
-    def go(self):
-        self.info('provisioning localhost')
-
-    def save(self):
-        self.info('saving localhost')
+    def execute(self, command):
+        self.run(command)
