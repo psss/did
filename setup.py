08507a1f9edce3e0b0392b3a77720571392cc007
Petr Šplíchal <psplicha@redhat.com>
2019-12-10 14:02:41 +0100

    Enable Python 3.8 in Travis, update metadata

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Office/Business',
         'Topic :: Utilities',
     ],
