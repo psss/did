39f8d540d91ec126636a4478c967b84182ff7398
Petr Šplíchal <psplicha@redhat.com>
2020-11-24 13:39:56 +0100

    Adjust the new 'html' report method
    Include plan name and summary on the page.
    Fix the table heading html syntax.
    Support short option -o as well.
    Use Common.run() to execute commands.
    Plus a bunch of minor style adjustments.

--- tmt/steps/report/html.py
+++ tmt/steps/report/html.py
@@ -1,24 +1,16 @@
 import os
-
-import jinja2
 import os.path
-import subprocess
+
+import click
+import jinja2
 
 import tmt
-import click
 
-class ReportHTML(tmt.steps.report.ReportPlugin):
-    """
-    Write HTML report of  results
-    """
 
-    # Supported methods
-    _methods = [tmt.steps.Method(name='html', doc=__doc__, order=50)]
-
-    HTML_TEMPLATE = """
+HTML_TEMPLATE = """
 <!DOCTYPE html>
 <head>
-    <title>Test results of {{ plan_name }}</title>
+    <title>Test results of {{ plan.name }}</title>
     <style>
         td.pass {
             background: green;
@@ -39,81 +31,99 @@ class ReportHTML(tmt.steps.report.ReportPlugin):
 </head>
 <body>
 
+<h1>{{ plan.name }}</h1>
+{% if plan.summary %}<h2>{{ plan.summary }}</h2>{% endif %}
 {% if results %}
 <table>
     <thead>
-        <td>Result</td>
-        <td>Name</td>
-        <td>Log files </td>
+        <tr>
+            <th>Result</th>
+            <th>Test</th>
+            <th>Logs</th>
+        </tr>
     </thead>
-    {% for res in results %}
+    {% for result in results %}
     <tr class="result {{ loop.cycle('odd', 'even') }}">
-        <td class="result {{ res.result|e }}"> {{ res.result|e }}</td>
-        <td class="name"> {{ res.name|e }}</td>
+        <td class="result {{ result.result|e }}"> {{ result.result|e }}</td>
+        <td class="name"> {{ result.name|e }}</td>
         <td class="log">
-        {% for log in res.log %}
-            <a href="{{base_dir }}/{{ log }}">{{ log | basename }}</a>
+        {% for log in result.log %}
+            <a href="{{ base_dir }}/{{ log }}">{{ log | basename }}</a>
         {% endfor %}
         </td>
     </tr>
     {% endfor %}
 </table>
 {% else %}
-<b>No results found</b>
+<b>No test results found.</b>
 {% endif %}
 </body>
 </html>
 """.strip()
 
+
+class ReportHTML(tmt.steps.report.ReportPlugin):
+    """ Format test results into an html report """
+
+    # Supported methods
+    _methods = [tmt.steps.Method(name='html', doc=__doc__, order=50)]
+
     @classmethod
     def options(cls, how=None):
-        """ Prepare command line options for connect """
+        """ Prepare command line options for the html report """
         return [
             click.option(
-                '--open', is_flag=True,
-                help='Open results in your preferred web browser'),
+                '-o', '--open', is_flag=True,
+                help='Open results in your preferred web browser.'),
             ] + super().options(how)
 
     def go(self):
         """ Process results """
         super().go()
 
-        env = jinja2.Environment()
-        env.filters["basename"] = lambda x: os.path.basename(x)
+        # Prepare the template
+        environment = jinja2.Environment()
+        environment.filters["basename"] = lambda x: os.path.basename(x)
+        template = environment.from_string(HTML_TEMPLATE)
 
-        template_to_render = env.from_string(self.HTML_TEMPLATE)
-
-        file_name = 'index.html'
-        wrote = self.write(file_name, data=template_to_render.render(
+        # Write the report
+        filename = 'index.html'
+        self.write(
+            filename,
+            data=template.render(
                 results=self.step.plan.execute.results(),
                 base_dir=self.step.plan.execute.workdir,
-                plan_name=self.step.plan.name
-                ))
-        if wrote is None: # --dry mode
+                plan=self.step.plan))
+
+        # Nothing more to do in dry mode
+        if self.opt('dry'):
             return
 
-        target = os.path.join(self.workdir, file_name)
-        self.info("Wrote report to", target)
+        # Show output file path
+        target = os.path.join(self.workdir, filename)
+        self.info("output", target, color='yellow')
         if not self.opt('open'):
             return
 
-        # opening target using xdg-open, fallback to $BROWSER
+        # Open target using xdg-open
         try:
-            process = subprocess.Popen(["xdg-open", target])
+            self.run(f"xdg-open {target}")
+            self.info('open', 'xdg-open', color='green')
             return
-        except OSError as err:
-            self.debug("Attempt to open via xdg-open not successful")
-            self.debug(f"Returned exception: {err}")
+        except tmt.utils.RunError:
+            self.debug(f"Open via 'xdg-open' not successful.")
+
+        # Fallback to the $BROWSER variable
         try:
             browser = os.environ.get('BROWSER')
             if not browser:
                 browser = 'unset'
-                raise KeyError("BROWSER empty")
-            process = subprocess.Popen([browser, target])
+                raise tmt.utils.ReportError(
+                    "Environment variable $BROWSER not defined.")
+            self.run(f"{browser} {target}")
+            self.info('open', browser, color='green')
             return
-        except (OSError, KeyError) as err:
-            self.debug(f"Attempt to open via $BROWSER ({browser}) not successful")
-            self.debug(f"Returned exception: {err}")
-
-
-        self.fail("Wasn't able to open browser, xdg-open nor $BROWSER worked")
+        except tmt.utils.GeneralError:
+            self.debug(f"Open via $BROWSER '{browser}' not successful.")
+            self.fail("Unable to open browser, xdg-open nor $BROWSER worked.")
+            raise
