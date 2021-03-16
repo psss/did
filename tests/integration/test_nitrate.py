ebb4638bf88001e396f0143fee1bbab8d5d687dd
Jan Ščotka <jscotka@redhat.com>
2021-03-16 17:58:55 +0100

    Improve debugging of tests using click runner

--- tests/integration/test_nitrate.py
+++ tests/integration/test_nitrate.py
@@ -16,19 +16,26 @@ TEST_DIR = Path(__file__).parent
 NITRATE_EXAMPLE = TEST_DIR / "data" / "nitrate"
 
 
-# General test plan for this component is: TP#29309
-class NitrateExport(RequreTestCase):
-
+class Base(RequreTestCase):
     def setUp(self):
         super().setUp()
-        self.tmpdir = Path(tempfile.mktemp())
+        self.tmpdir = Path(tempfile.mktemp(prefix=str(TEST_DIR)))
         shutil.copytree(NITRATE_EXAMPLE, self.tmpdir)
         self.cwd = os.getcwd()
+        self.runner_output = None
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
         os.chdir(self.cwd)
         super().tearDown()
+        if hasattr(self.runner_output, "exit_code") and self.runner_output.exit_code != 0:
+            print("Return code:", self.runner_output.exit_code)
+            print("Output:", self.runner_output.output)
+            print("Exception:", self.runner_output.exception)
+
+
+# General test plan for this component is: TP#29309
+class NitrateExport(Base):
 
     def test_create(self):
         fmf_node = Tree(self.tmpdir).find("/new_testcase")
@@ -36,9 +43,8 @@ class NitrateExport(RequreTestCase):
 
         os.chdir(self.tmpdir / "new_testcase")
         runner = CliRunner()
-        output = runner.invoke(tmt.cli.main, [
+        self.runner_output = runner.invoke(tmt.cli.main, [
             "test", "export", "--nitrate", "--create", "--general", "."])
-        print(output)
         # Reload the node data to see if it appears there
         fmf_node = Tree(self.tmpdir).find("/new_testcase")
         self.assertIn("extra-nitrate", fmf_node.data)
@@ -49,39 +55,30 @@ class NitrateExport(RequreTestCase):
 
         os.chdir(self.tmpdir / "existing_testcase")
         runner = CliRunner()
-        runner.invoke(tmt.cli.main,
-            ["test", "export", "--nitrate", "--create", "--general", "."])
+        self.runner_output = runner.invoke(tmt.cli.main,
+                                           ["test", "export", "--nitrate",
+                                            "--create", "--general", "."])
         fmf_node = Tree(self.tmpdir).find("/existing_testcase")
 
         self.assertEqual(fmf_node.data["extra-nitrate"], "TC#0609686")
 
 
-class NitrateImport(RequreTestCase):
-
-    def setUp(self):
-        super().setUp()
-        self.tmpdir = Path(tempfile.mktemp())
-        shutil.copytree(NITRATE_EXAMPLE, self.tmpdir)
-
-    def tearDown(self):
-        shutil.rmtree(self.tmpdir)
-        super().tearDown()
+class NitrateImport(Base):
 
     def test_import_manual_confirmed(self):
         runner = CliRunner()
         # TODO: import does not respect --root param anyhow (could)
-        result = runner.invoke(tmt.cli.main,
+        self.runner_output = runner.invoke(tmt.cli.main,
             ['-vvvvdddd', '--root', self.tmpdir / "import_case", "test",
                 "import", "--nitrate", "--manual", "--case=609704"])
-        print(result.output)
-        self.assertEqual(result.exit_code, 0)
-        self.assertIn("Importing the 'Imported_Test_Case'", result.output)
-        self.assertIn("test case found 'TC#0609704'", result.output)
-        self.assertIn("Metadata successfully stored into", result.output)
+        self.assertEqual(self.runner_output.exit_code, 0)
+        self.assertIn("Importing the 'Imported_Test_Case'", self.runner_output.output)
+        self.assertIn("test case found 'TC#0609704'", self.runner_output.output)
+        self.assertIn("Metadata successfully stored into", self.runner_output.output)
         filename = next(filter(lambda x:
                                "Metadata successfully stored into"
                                in x and "main.fmf" in x,
-                               result.output.splitlines())).split("'")[1]
+                               self.runner_output.output.splitlines())).split("'")[1]
         # /home/jscotka/git/tmt/Manual/Imported_Test_Case/main.fmf
         # TODO: not possible to specify, where to store data,
         # it always creates Manual subdir, I do not want it.
@@ -94,20 +91,20 @@ class NitrateImport(RequreTestCase):
 
     def test_import_manual_proposed(self):
         runner = CliRunner()
-        result = runner.invoke(
+        self.runner_output = runner.invoke(
             tmt.cli.main, ['--root', self.tmpdir / "import_case", "test",
                            "import", "--nitrate", "--manual", "--case=609705"])
-        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(self.runner_output.exit_code, 0)
         # TODO: This is strange, expect at least some output in
         # case there is proper case, just case is not CONFIRMED
         # I can imagine also e.g. at least raise error but not pass,
         # with no output
-        self.assertEqual(result.output.strip(), "")
+        self.assertEqual(self.runner_output.output.strip(), "")
         fmf_node = Tree(self.tmpdir).find("/import_case")
         self.assertEqual(fmf_node, None)
 
 
-class NitrateImportAutomated(RequreTestCase):
+class NitrateImportAutomated(Base):
     test_md_content= """# Setup
 Do this and that to setup the environment.
 
@@ -162,16 +159,6 @@ extra-nitrate: TC#0609926
 extra-summary: /tmt/integration
 extra-task: /tmt/integration
 """
-    def setUp(self):
-        super().setUp()
-        self.tmpdir = Path(tempfile.mktemp())
-        shutil.copytree(NITRATE_EXAMPLE, self.tmpdir)
-        self.cwd = os.getcwd()
-
-    def tearDown(self):
-        os.chdir(self.cwd)
-        shutil.rmtree(self.tmpdir)
-        super().tearDown()
 
     def test_basic(self):
         os.chdir(self.tmpdir / "import_case_automated")
@@ -180,9 +167,9 @@ extra-task: /tmt/integration
         self.assertNotIn("main.fmf", files)
         self.assertNotIn("test.md", files)
         runner = CliRunner()
-        result = runner.invoke(
+        self.runner_output = runner.invoke(
             tmt.cli.main, [ "test", "import", "--nitrate"])
-        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(self.runner_output.exit_code, 0)
         files = os.listdir()
         self.assertIn("Makefile", files)
         self.assertIn("test.md", files)
