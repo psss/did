52308530fe5f7ba40bc22a525da6ae44b62f7b92
Petr Šplíchal <psplicha@redhat.com>
2021-04-26 19:04:33 +0200

    Adjust the simple test for ansible prepare
    Fix test script to actually run the test.
    Include the metadata tree root.
    Simplify the test data a bit.

--- tests/prepare/ansible/test.sh
+++ tests/prepare/ansible/test.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 rlJournalStart
     rlPhaseStartSetup
         rlRun "pushd data"
-        rlRun "set -o pipefail"
     rlPhaseEnd
 
-    for image in fedora centos:7 ; do
-        # Prepare the tmt command and expected error message
-        tmt="tmt run -av provision -h container -i $image"
+    for image in fedora centos:7 centos:8; do
+        rlPhaseStartTest $image
+            rlRun "tmt run -arv provision -h container -i $image"
+        rlPhaseEnd
     done
 
     rlPhaseStartCleanup
