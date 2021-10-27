73b98fcb76c6034602f1aebdac3884df57f0991a
Milos Malik <mmalik@redhat.com>
2021-10-27 11:21:16 +0200

    Use user email for searching updated jira issues
    After the recent changes Jira issues are found when the python
    searches use `user.email` instead of `user.login`.

--- did/plugins/jira.py
+++ did/plugins/jira.py
@@ -181,7 +181,7 @@ class JiraUpdated(Stats):
             query = (
                 "issueFunction in commented"
                 "('by {0} after {1} before {2}')".format(
-                    self.parent.login or self.user.login,
+                    self.parent.login or self.user.email,
                     self.options.since, self.options.until))
             if self.parent.project:
                 query = query + " AND project = '{0}'".format(
