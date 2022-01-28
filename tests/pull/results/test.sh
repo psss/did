psss/did

commit ec1d88bdcf5df1ad4c9040dfe72ee88f29cb03fe

Add guest.pull() to the finish step

tmt/steps/finish/__init__.py
@@ -71,6 +71,8 @@ class Finish(tmt.steps.Step):
             guest_copy.parent = self
             for plugin in self.plugins():
                 plugin.go(guest_copy)
+            # Pull logs from guest
+            guest.pull()
 
         # Stop and remove provisioned guests
         for guest in self.plan.provision.guests():

tests/finish/ansible/data/plan.fmf
@@ -15,6 +15,10 @@ finish:
       order: 70
       how: shell
       script: bash -xc "[ -f /tmp/finished ]"
+    - name: Create a file that is pulled during the finish stage
+      order: 80
+      how: shell
+      script: touch my_file.txt
 execute:
     how: tmt
     script: echo fake

tests/finish/ansible/test.sh
@@ -4,16 +4,21 @@
 rlJournalStart
     rlPhaseStartSetup
         rlRun "pushd data"
+        # Create a temporary directory used for testing
+        rlRun "tmp=\$(mktemp -d)" 0 "Creating tmp directory"
     rlPhaseEnd
 
     for method in ${METHODS:-container}; do
         rlPhaseStartTest "Test ($method)"
-            rlRun "tmt run -arv provision -h $method"
+            rlRun "tmt run -i $tmp --scratch -av provision -h $method"
+            # Check that created file is synced back
+            rlRun "ls -l $tmp/plan/tree"
+            rlAssertExists "$tmp/plan/tree/my_file.txt"
 
             # For container provision try centos images as well
             if [[ $method == container ]]; then
-                rlRun "tmt run -arv finish provision -h $method -i centos:7"
-                rlRun "tmt run -arv finish provision -h $method -i centos:8"
+                rlRun "tmt run -i $tmp --scratch -av finish provision -h $method -i centos:7"
+                rlRun "tmt run -i $tmp --scratch -av finish provision -h $method -i centos:8"
             fi
 
             # After the local provision remove the test file
@@ -24,6 +29,7 @@ rlJournalStart
     done
 
     rlPhaseStartCleanup
+        rlRun "rm -r $tmp" 0 "Removing tmp directory"
         rlRun "popd"
     rlPhaseEnd
 rlJournalEnd

commit a52425552da98364e108b75e9bc2900da8a589aa

Implement variables for storing logs and artifacts
Introduce `TMT_TEST_DATA` and `TMT_PLAN_DATA` variables which can
be used during `prepare`, `execute` and `finish` step for storing
artifacts such as logs into a directory which is pulled back from
the guest for further inspection. Add relevant test coverage.

This change also fixes regression introduced by ec1d88b which
caused the `results.yaml` file to be overridden by empty results
file fetched back from the guest by pulling only selected
directories instead of the whole plan workdir.

It should also cover #1011 by pulling logs only if any `prepare`,
`execute` or `finish` plugin has been executed. However, a proper
fix which would keep track of the pushed state and ensure that the
whole workdir content is pushed only once is still needed.

tmt/base.py
@@ -555,12 +555,19 @@ class Plan(Core):
 
     def __init__(self, node, run=None):
         """ Initialize the plan """
+        super().__init__(node, parent=run)
+
+        # Save the run, prepare worktree and plan data directory
         self.my_run = run
+        if self.my_run:
+            self._initialize_worktree()
+            self._initialize_data_directory()
+
+        # Store 'environment' and 'environment-file' keys content
         self._environment = self._get_environment_vars(node)
         # Expand all environment variables in the node
         with tmt.utils.modify_environ(self.environment):
             self._expand_node_data(node.data)
-        super().__init__(node, parent=run)
 
         # Initialize test steps
         self.discover = tmt.steps.discover.Discover(
@@ -575,9 +582,6 @@ class Plan(Core):
             self.node.get('report'), self)
         self.finish = tmt.steps.finish.Finish(
             self.node.get('finish'), self)
-
-        # Initialize workdir tree
-        self._initialize_worktree()
 
         # Relevant gates (convert to list if needed)
         self.gate = node.get('gate')
@@ -605,10 +609,12 @@ class Plan(Core):
     @property
     def environment(self):
         """ Return combined environment from plan data and command line """
-        if self.my_run and self.my_run.environment:
+        if self.my_run:
             combined = self._environment.copy()
             # Command line variables take precedence
             combined.update(self.my_run.environment)
+            # Include path to the plan data directory
+            combined["TMT_PLAN_DATA"] = self.data_directory
             return combined
         else:
             return self._environment
@@ -639,24 +645,33 @@ class Plan(Core):
 
         Used as cwd in prepare, execute and finish steps.
         """
-        # Bail out if workdir does not exist (not running the plan)
-        if not self.workdir:
-            return
-
         self.worktree = os.path.join(self.workdir, 'tree')
         tree_root = self.my_run.tree.root
 
         # Create an empty directory if there's no metadata tree
         if not tree_root:
-            self.debug('Create an empty worktree (no metadata tree).')
+            self.debug('Create an empty worktree (no metadata tree).', level=2)
             os.makedirs(self.worktree, exist_ok=True)
             return
 
         # Sync metadata root to the worktree
-        self.debug(f"Sync the worktree to '{self.worktree}'.")
+        self.debug(f"Sync the worktree to '{self.worktree}'.", level=2)
         self.run([
             "rsync", "-ar", "--exclude", ".git",
             f"{tree_root}/", self.worktree], shell=False)
+
+    def _initialize_data_directory(self):
+        """
+        Create the plan data directory
+
+        This is used for storing logs and other artifacts created during
+        prepare step, test execution or finish step and which are pulled
+        from the guest for possible future inspection.
+        """
+        self.data_directory = os.path.join(self.workdir, "data")
+        self.debug(
+            f"Create the data directory '{self.data_directory}'.", level=2)
+        os.makedirs(self.data_directory, exist_ok=True)
 
     def _fmf_context(self):
         """ Return combined context from plan data and command line """

tmt/steps/execute/__init__.py
@@ -240,7 +240,7 @@ class ExecutePlugin(tmt.steps.Plugin):
         directory = os.path.join(
             self.step.workdir, TEST_DATA, test.name.lstrip('/'))
         if create and not os.path.isdir(directory):
-            os.makedirs(directory)
+            os.makedirs(os.path.join(directory, TEST_DATA))
         if not filename:
             return directory
         path = os.path.join(directory, filename)

tmt/steps/execute/detach.py
@@ -135,7 +135,8 @@ class ExecuteDetach(tmt.steps.execute.ExecutePlugin):
                 raise
             end = time.time()
             # Pull logs from guest, show logs and check results
-            guest.pull()
+            guest.pull(source=self.step.plan.execute.workdir)
+            guest.pull(source=self.step.plan.data_directory)
             self.show_logs()
             # If --exit-first is used, not all tests may have been executed.
             # run.sh writes a letter F/. (Fail/pass) to stdout for each test.

tmt/steps/execute/internal.py
@@ -144,6 +144,8 @@ class ExecuteInternal(tmt.steps.execute.ExecutePlugin):
         data_directory = self.data_path(test, full=True, create=True)
         environment = test.environment.copy()
         environment["TMT_TREE"] = self.parent.plan.worktree
+        environment["TMT_TEST_DATA"] = os.path.join(
+            data_directory, tmt.steps.execute.TEST_DATA)
         if test.framework == 'beakerlib':
             environment['BEAKERLIB_DIR'] = data_directory
             environment['BEAKERLIB_COMMAND_SUBMIT_LOG'] = (
@@ -328,6 +330,9 @@ class ExecuteInternal(tmt.steps.execute.ExecutePlugin):
                     index += 1
                 # Overwrite the progress bar, the test data is irrelevant
                 self._show_progress('', '', True)
+            # Pull artifacts created in the plan data directory
+            self.debug("Pull the plan data directory.", level=2)
+            guest.pull(source=self.step.plan.data_directory)
 
     def results(self):
         """ Return test results """

tmt/steps/finish/__init__.py
@@ -71,8 +71,10 @@ class Finish(tmt.steps.Step):
             guest_copy.parent = self
             for plugin in self.plugins():
                 plugin.go(guest_copy)
-            # Pull logs from guest
-            guest.pull()
+            # Pull artifacts created in the plan data directory
+            # if there was at least one plugin executed
+            if self.plugins():
+                guest_copy.pull(self.plan.data_directory)
 
         # Stop and remove provisioned guests
         for guest in self.plan.provision.guests():

tmt/steps/prepare/__init__.py
@@ -102,6 +102,10 @@ class Prepare(tmt.steps.Step):
             # Execute each prepare plugin
             for plugin in self.plugins():
                 plugin.go(guest_copy)
+            # Pull artifacts created in the plan data directory
+            # if there was at least one plugin executed
+            if self.plugins():
+                guest_copy.pull(self.plan.data_directory)
 
         # Give a summary, update status and save
         self.summary()

tests/core/environment-file/test.sh
@@ -33,7 +33,6 @@ rlJournalStart
 
     rlPhaseStartTest "Empty environment file"
         rlRun "tmt run -rvvddd discover finish plan -n empty 2>&1 | tee output"
-        rlAssertGrep "environment: {}" "output"
         rlAssertGrep "WARNING.*Empty environment file" "output"
     rlPhaseEnd
 

tests/pull/results/data/.fmf/version
@@ -1,0 +1,1 @@
+1

tests/pull/results/data/plan.fmf
@@ -1,0 +1,6 @@
+discover:
+    how: fmf
+provision:
+    how: local
+execute:
+    how: tmt

tests/pull/results/data/test.fmf
@@ -1,0 +1,11 @@
+/good:
+    test: |
+        echo everything ok
+        echo extra good > $TMT_TEST_DATA/extra.log
+        echo common good >> $TMT_PLAN_DATA/log.txt
+/bad:
+    test: |
+        echo everything ko
+        echo extra bad > $TMT_TEST_DATA/extra.log
+        echo common bad >> $TMT_PLAN_DATA/log.txt
+        /bin/false

tests/pull/results/main.fmf
@@ -1,0 +1,12 @@
+summary: Test results and logs are pulled from guest
+description:
+    Check that test logs are correctly fetched from the guest,
+    test results are stored into `results.yaml` and plan data
+    directory content is synced back.
+
+environment:
+    METHODS: container local
+adjust:
+  - when: how == full
+    environment:
+        METHODS: container virtual local

tests/pull/results/test.sh
@@ -1,0 +1,37 @@
+#!/bin/bash
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "run=\$(mktemp -d)" 0 "Create run directory"
+        rlRun "pushd data"
+    rlPhaseEnd
+
+    for method in ${METHODS:-local}; do
+        rlPhaseStartTest "Test $method"
+            # Run the plan, check for expected results
+            rlRun -s "tmt run -av --scratch --id $run provision -h $method" 1
+            rlAssertGrep "1 test passed and 1 test failed" $rlRun_LOG
+
+            # Check output and extra logs in the test data directory
+            data="$run/plan/execute/data"
+            rlAssertGrep "ok" "$data/test/good/output.txt"
+            rlAssertGrep "ko" "$data/test/bad/output.txt"
+            rlAssertGrep "extra good" "$data/test/good/data/extra.log"
+            rlAssertGrep "extra bad" "$data/test/bad/data/extra.log"
+
+            # Check logs in the plan data directory
+            rlAssertGrep "common good" "$run/plan/data/log.txt"
+            rlAssertGrep "common bad" "$run/plan/data/log.txt"
+
+            # Check report of the last run for correct results
+            rlRun -s "tmt run --last report" 1
+            rlAssertGrep "1 test passed and 1 test failed" $rlRun_LOG
+        rlPhaseEnd
+    done
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -rf $run" 0 "Remove run directory"
+    rlPhaseEnd
+rlJournalEnd

tests/pull/simple/main.fmf
@@ -1,0 +1,14 @@
+summary: Simple provision & finish works fine
+description:
+    Make sure that provisioning a guest and clean it up works as
+    expected even if no preparation, test execution or finish step
+    scripts are performed.
+link:
+  - verifies: https://github.com/psss/tmt/issues/1011
+
+environment:
+    METHODS: container local
+adjust:
+  - when: how == full
+    environment:
+        METHODS: container virtual local

tests/pull/simple/test.sh
@@ -1,0 +1,26 @@
+#!/bin/bash
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "run=\$(mktemp -d)" 0 "Create run directory"
+        rlRun "tmp=\$(mktemp -d)" 0 "Create tmt directory"
+        rlRun "pushd $tmp"
+    rlPhaseEnd
+
+    for method in ${METHODS:-local}; do
+        rlPhaseStartTest "Test one step ($method)"
+            rlRun "tmt run -i $run --scratch provision -h $method finish"
+        rlPhaseEnd
+
+        rlPhaseStartTest "Test two steps ($method)"
+            rlRun "tmt run -i $run --scratch provision -h $method"
+            rlRun "tmt run -i $run finish"
+        rlPhaseEnd
+    done
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -rf $run $tmp" 0 "Remove run & tmp directory"
+    rlPhaseEnd
+rlJournalEnd