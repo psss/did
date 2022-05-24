5884f4b6df0422eb7eabdfaaf05ea632fcb4fc0f
Miroslav Vadkerti <mvadkert@redhat.com>
2022-05-24 19:15:55 +0200

    Add schemas for plans
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- tests/unit/test_schemas.py
+++ tests/unit/test_schemas.py
@@ -27,12 +27,30 @@ def stories_schema():
 
 
 @pytest.fixture
+def plans_schema():
+    # TODO: tmt package shall provide a helper function
+    # for "load schemas for test/plan/story"
+    schema_file = os.path.join(SCHEMADIR, 'plan.yaml')
+    return YAML(typ="safe").load(open(schema_file, encoding="utf-8"))
+
+
+@pytest.fixture
 def schema_store():
     # TODO: tmt package shall provide a helper function
     # for "load schemas for test/plan/story"
     store = {}
 
-    for schema_name in ('common', 'core'):
+    for schema_name in (
+            'common', 'core',
+            'discover/fmf', 'discover/shell',
+            'execute/tmt', 'execute/upgrade',
+            'finish/ansible', 'finish/shell',
+            'prepare/ansible', 'prepare/install', 'prepare/shell',
+            'provision/artemis', 'provision/connect', 'provision/container',
+            'provision/hardware', 'provision/local', 'provision/virtual',
+            'report/display', 'report/html',
+            'test'
+            ):
         schema_file = os.path.join(SCHEMADIR, f'{schema_name}.yaml')
         schema = YAML(typ="safe").load(
             open(schema_file, encoding="utf-8"))
@@ -53,6 +71,12 @@ def story_validation_result(request, schema_store, stories_schema):
     return node.name, node.validate(stories_schema, schema_store)
 
 
+@pytest.fixture(params=tmt.Tree(ROOTDIR).plans())
+def plan_validation_result(request, schema_store, plans_schema):
+    node = request.param.node
+    return node.name, node.validate(plans_schema, schema_store)
+
+
 def test_tests_schema(test_validation_result):
     name, result = test_validation_result
     if not result.result:
@@ -69,3 +93,12 @@ def test_stories_schema(story_validation_result):
             print(error)
 
     assert result.result, f'Story {name} fails validation'
+
+
+def test_plans_schema(plan_validation_result):
+    name, result = plan_validation_result
+    if not result.result:
+        for error in result.errors:
+            print(error)
+
+    assert result.result, f'Plan {name} fails validation'
