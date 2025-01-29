42060a954f6e82520780201b6bc40879b5f53c4c
Sandro Bonazzola <sbonazzo@redhat.com>
2025-01-29 09:24:34 +0100

    Cover markdown format while testing Koji
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- tests/plugins/test_koji.py
+++ tests/plugins/test_koji.py
@@ -39,6 +39,13 @@ def test_koji_build():
     stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[0].stats
     assert any(["doctl-1.65.0-1.fc34" in str(stat)
                for stat in stats])
+    stats = did.cli.main(
+        option + INTERVAL + " --format=markdown"
+        )[0][0].stats[0].stats[0].stats
+    assert any([
+        "[doctl-1.65.0-1.fc34](https://koji.fedoraproject.org/"
+        in str(stat)
+        for stat in stats])
     stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[0].stats
     assert not stats
     stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[0].stats
