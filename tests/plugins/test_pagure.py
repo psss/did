dea21762cd75a5b57ddb192aab24f333d3954cf3
Sandro Bonazzola <sbonazzo@redhat.com>
2025-02-07 15:11:13 +0100

    Add collection of comments for Pagure
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- tests/plugins/test_pagure.py
+++ tests/plugins/test_pagure.py
@@ -28,6 +28,12 @@ url = https://pagure.io/api/0/
 login = psss
 """
 
+# Indexes within stats
+ISSUE_CREATED = 0
+ISSUE_CLOSED = 1
+PR_CREATED = 2
+COMMENTS = 3
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #  Tests
@@ -37,11 +43,11 @@ def test_pagure_issues_created():
     """ Created issues """
     did.base.Config(CONFIG)
     option = "--pagure-issues-created "
-    stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[0].stats
+    stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[ISSUE_CREATED].stats
     assert any(["did#1 - Open Issue" in str(stat) for stat in stats])
-    stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[0].stats
+    stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[ISSUE_CREATED].stats
     assert not stats
-    stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[0].stats
+    stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[ISSUE_CREATED].stats
     assert not stats
 
 
@@ -49,11 +55,11 @@ def test_pagure_issues_closed():
     """ Closed issues """
     did.base.Config(CONFIG)
     option = "--pagure-issues-closed "
-    stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[1].stats
+    stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[ISSUE_CLOSED].stats
     assert any(["did#2 - Closed Issue" in str(stat) for stat in stats])
-    stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[1].stats
+    stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[ISSUE_CLOSED].stats
     assert not stats
-    stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[1].stats
+    stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[ISSUE_CLOSED].stats
     assert not stats
 
 
@@ -61,13 +67,28 @@ def test_pagure_pull_requests_created():
     """ Created pull requests """
     did.base.Config(CONFIG)
     option = "--pagure-pull-requests-created "
-    stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[2].stats
+    stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[PR_CREATED].stats
     assert any(
         ["did#3 - Open Pull Request" in str(stat) for stat in stats])
-    stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[2].stats
+    stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[PR_CREATED].stats
     assert not stats
-    stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[2].stats
+    stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[PR_CREATED].stats
     assert not stats
+
+
+def test_pagure_comments():
+    """ Comments """
+    did.base.Config(CONFIG)
+    option = "--pagure-commented "
+    stats = did.cli.main(option + INTERVAL)[0][0].stats[0].stats[COMMENTS].stats
+    assert any(
+        ["2018-11-26 - psss commented on PR" in str(stat) for stat in stats])
+    stats = did.cli.main(option + BEFORE)[0][0].stats[0].stats[COMMENTS].stats
+    assert any(
+        ["2018-11-22 - psss commented on PR" in str(stat) for stat in stats])
+    stats = did.cli.main(option + AFTER)[0][0].stats[0].stats[COMMENTS].stats
+    assert any(
+        ["2018-11-27 - psss commented on issue" in str(stat) for stat in stats])
 
 
 def test_pagure_missing_url():
