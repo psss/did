67ec969d9384a06f3fa8e39fe99028c251551ab2
Sandro Bonazzola <sbonazzo@redhat.com>
2025-02-12 15:11:34 +0100

    Increased confluence test coverage
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- tests/plugins/test_confluence.py
+++ tests/plugins/test_confluence.py
@@ -1,18 +1,16 @@
 # coding: utf-8
 """ Tests for the Confluence plugin """
 
-import os
-import sys
+import pytest
 
 import did.base
 import did.cli
-from did.base import ReportError
 from did.plugins.confluence import ConfluenceStats
 
-sys.path.insert(1, os.path.join(os.path.dirname(__file__), "..", ".."))
-
-
 CONFIG = """
+[general]
+email = mail@example.com
+
 [confluence]
 type = confluence
 url = https://confluence.automotivelinux.org/
@@ -24,26 +22,36 @@ url = https://confluence.automotivelinux.org/
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-def assert_conf_error(config, expected_error=ReportError):
+def assert_conf_error(config, expected_error=did.base.ReportError):
     """ Test given config and check that given error type is raised """
-    print(config)
     did.base.Config(config)
-    dict(did.base.Config().section("confluence"))
-    error = None
-    try:
+    with pytest.raises(expected_error):
         ConfluenceStats("confluence")
-        print("pippo")
-        print(ConfluenceStats)
-    except ReportError as e:
-        error = e
-        print(error)
-    assert isinstance(error, expected_error)
+
+
+def test_config_auth_url():
+    """  Test authentication url in configuration """
+    did.base.Config(f"""
+{CONFIG}
+auth_url = https://confluence.automotivelinux.org/step-auth-gss
+""")
+    ConfluenceStats("confluence")
 
 
 def test_config_gss_auth():
     """  Test default authentication configuration """
     did.base.Config(CONFIG)
     ConfluenceStats("confluence")
+
+
+def test_wrong_auth():
+    """  Test wrong authentication type configuration """
+    did.base.Config(f"""
+{CONFIG}
+auth_type = OAuth2
+""")
+    with pytest.raises(did.base.ReportError, match=r"Unsupported authentication type"):
+        ConfluenceStats("confluence")
 
 
 def test_config_basic_auth():
@@ -101,9 +109,53 @@ auth_password_file = ~/.did/config
 """)
 
 
-def test_config_invaliad_ssl_verify():
+def test_confluence_config_invaliad_ssl_verify():
     """  Test ssl_verify with wrong bool value """
     assert_conf_error(f"""
 {CONFIG}
 ssl_verify = ss
 """)
+
+
+def test_confluence_ssl_verify():
+    """Test ssl_verify """
+    did.base.Config(f"""
+{CONFIG}
+ssl_verify = False
+""")
+    with pytest.raises(did.base.ReportError, match=r"Confluence authentication failed"):
+        # expected to fail authentication as we are not providing valid
+        # credentials
+        did.cli.main("today")
+
+
+def test_confluence_missing_url():
+    """ Missing URL """
+    assert_conf_error(
+        CONFIG.replace(
+            "url = https://confluence.automotivelinux.org/\n",
+            ""))
+
+
+def test_confluence_wrong_url():
+    """ Missing URL """
+    did.base.Config(f"""{did.base.Config.example()}
+[confluence]
+type = confluence
+url = https://localhost
+""")
+    with pytest.raises(did.base.ReportError, match=r"Failed to connect to Confluence"):
+        did.cli.main("today")
+
+
+def test_missing_token():
+    """ `token` and `token_file` missing with token auth """
+    did.base.Config(f"""
+{CONFIG}
+auth_type = token
+token_expiration = 30
+""")
+    with pytest.raises(
+            did.base.ReportError, match=r"The `token` or `token_file` key must be set"
+            ):
+        ConfluenceStats("confluence")
