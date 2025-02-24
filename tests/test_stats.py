27046d60a8bd6681f8074d8d04a9c482d2f6690c
Sandro Bonazzola <sbonazzo@redhat.com>
2025-02-24 15:01:13 +0100

    Fix pylint C0415
    Fixes:
    C0415: Import outside toplevel (...) (import-outside-toplevel)
    
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,30 +1,23 @@
 # coding: utf-8
 
+import did.stats
+
+
 def test_Stats():
-    # simple test that import works
-    from did.stats import Stats
-    assert Stats
+    assert did.stats.Stats
 
 
 def test_StatsGroup():
-    # simple test that import works
-    from did.stats import StatsGroup
-    assert StatsGroup
+    assert did.stats.StatsGroup
 
 
 def test_UserStats():
-    # simple test that import works
-    from did.stats import UserStats
-    assert UserStats
+    assert did.stats.UserStats
 
 
 def test_EmptyStats():
-    # simple test that import works
-    from did.stats import EmptyStats
-    assert EmptyStats
+    assert did.stats.EmptyStats
 
 
 def test_EmptyStatsGroup():
-    # simple test that import works
-    from did.stats import EmptyStatsGroup
-    assert EmptyStatsGroup
+    assert did.stats.EmptyStatsGroup
