psss/did

commit 6fa3bdb8c583efc66614d9ff2fe17705e9548755

Fix issue with wrong `until` in GitHub search (#376)
* Fix issue with wrong `until` in GitHub search

* Use $did as `did --config` in tests

* Add tests that break in master with wrong --until

did/plugins/github.py
@@ -59,7 +59,7 @@ from did.utils import listed, log, pretty
 # Identifier padding
 PADDING = 3
 
-# Number of issues to be fetched per page
+# Number of GH items to be fetched per page
 PER_PAGE = 100
 
 # Default number of seconds waiting on GitHub before giving up
@@ -122,6 +122,11 @@ class GitHub():
                     valid_issues.append(issue)
                     break
         return valid_issues
+
+    @staticmethod
+    def until(until):
+        """Issue #362: until for GH should have - delta(day=1)"""
+        return until - 1
 
     def request(self, url):
         while True:
@@ -239,11 +244,10 @@ class IssuesCreated(Stats):
 
     def fetch(self):
         log.info("Searching for issues created by %s", self.user)
-        query = (
-            f"search/issues?q=author:{self.user.login}"
-            f"+created:{self.options.since}..{self.options.until}"
-            "+type:issue"
-            )
+        login = self.user.login
+        since = self.options.since
+        until = GitHub.until(self.options.until)
+        query = f"search/issues?q=author:{login}+created:{since}..{until}+type:issue"
         self.stats = [
             Issue(issue, self.parent) for issue in self.parent.github.search(query)]
 
@@ -253,11 +257,10 @@ class IssuesClosed(Stats):
 
     def fetch(self):
         log.info("Searching for issues closed by %s", self.user)
-        query = (
-            f"search/issues?q=assignee:{self.user.login}"
-            f"+closed:{self.options.since}..{self.options.until}"
-            "+type:issue"
-            )
+        login = self.user.login
+        since = self.options.since
+        until = GitHub.until(self.options.until)
+        query = f"search/issues?q=assignee:{login}+closed:{since}..{until}+type:issue"
         self.stats = [
             Issue(issue, self.parent) for issue in self.parent.github.search(query)]
 
@@ -267,17 +270,13 @@ class IssueCommented(Stats):
 
     def fetch(self):
         log.info("Searching for issues commented on by %s", self.user)
-        query = (
-            f"search/issues?q=commenter:{self.user.login}"
-            f"+updated:{self.options.since}..{self.options.until}"
-            "+type:issue"
-            )
+        login = self.user.login
+        since = self.options.since
+        until = GitHub.until(self.options.until)
+        query = f"search/issues?q=commenter:{login}+updated:{since}..{until}+type:issue"
         commented_issues = self.parent.github.search(query)
         valid_issues = self.parent.github.commented_in_range(
-            commented_issues,
-            self.options.since.datetime,
-            self.options.until.datetime,
-            self.user.login
+            commented_issues, since.datetime, until.datetime, login
             )
         self.stats = [
             Issue(issue, self.parent) for issue in valid_issues]
@@ -288,11 +287,10 @@ class PullRequestsCreated(Stats):
 
     def fetch(self):
         log.info("Searching for pull requests created by %s", self.user)
-        query = (
-            f"search/issues?q=author:{self.user.login}"
-            f"+created:{self.options.since}..{self.options.until}"
-            "+type:pr"
-            )
+        login = self.user.login
+        since = self.options.since
+        until = GitHub.until(self.options.until)
+        query = f"search/issues?q=author:{login}+created:{since}..{until}+type:pr"
         self.stats = [
             Issue(issue, self.parent) for issue in self.parent.github.search(query)]
 
@@ -302,17 +300,13 @@ class PullRequestsCommented(Stats):
 
     def fetch(self):
         log.info("Searching for pull requests commented on by %s", self.user)
-        query = (
-            f"search/issues?q=commenter:{self.user.login}"
-            f"+updated:{self.options.since}..{self.options.until}"
-            "+type:pr"
-            )
+        login = self.user.login
+        since = self.options.since
+        until = GitHub.until(self.options.until)
+        query = f"search/issues?q=commenter:{login}+updated:{since}..{until}+type:pr"
         commented_issues = self.parent.github.search(query)
         valid_issues = self.parent.github.commented_in_range(
-            commented_issues,
-            self.options.since.datetime,
-            self.options.until.datetime,
-            self.user.login
+            commented_issues, since.datetime, until.datetime, login
             )
         self.stats = [
             Issue(issue, self.parent) for issue in valid_issues]
@@ -323,11 +317,10 @@ class PullRequestsClosed(Stats):
 
     def fetch(self):
         log.info("Searching for pull requests closed by %s", self.user)
-        query = (
-            f"search/issues?q=assignee:{self.user.login}"
-            f"+closed:{self.options.since}..{self.options.until}"
-            "+type:pr"
-            )
+        login = self.user.login
+        since = self.options.since
+        until = GitHub.until(self.options.until)
+        query = f"search/issues?q=assignee:{login}+closed:{since}..{until}+type:pr"
         self.stats = [
             Issue(issue, self.parent) for issue in self.parent.github.search(query)]
 
@@ -337,11 +330,12 @@ class PullRequestsReviewed(Stats):
 
     def fetch(self):
         log.info("Searching for pull requests reviewed by %s", self.user)
+        login = self.user.login
+        since = self.options.since
+        until = GitHub.until(self.options.until)
         query = (
-            f"search/issues?q=reviewed-by:{self.user.login}"
-            f"+-author:{self.user.login}"
-            f"+closed:{self.options.since}..{self.options.until}"
-            "+type:pr"
+            f"search/issues?q=reviewed-by:{login}+-author:{login}"
+            f"+closed:{since}..{until}+type:pr"
             )
         self.stats = [
             Issue(issue, self.parent) for issue in self.parent.github.search(query)]

tests/github/issues.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
+did="did --config"
+
 YEAR_2021="--since 2021-01-01 --until 2021-12-31"
 YEAR_2022="--since 2022-01-01 --until 2022-12-31"
 YEAR_2023="--since 2023-01-01 --until 2023-12-31"
+CHECK_UNTIL="--since 2022-10-01 --until 2022-10-26"
 
 rlJournalStart
 
     # Issues Created
 
     rlPhaseStartTest "Issues Created"
-        rlRun -s "did --config ./config-default.ini --gh-issues-created $YEAR_2022"
+        rlRun -s "$did ./config-default.ini --gh-issues-created $YEAR_2022"
         rlAssertGrep "Issues created on gh: 31$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#1737 - Introduce a new step for cleanup tasks" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#149 - Checkout of the default branch fails" $rlRun_LOG
@@ -18,7 +21,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Created (org:teemtee)"
-        rlRun -s "did --config ./config-org.ini --gh-issues-created $YEAR_2022"
+        rlRun -s "$did ./config-org.ini --gh-issues-created $YEAR_2022"
         rlAssertGrep "Issues created on gh: 30$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#1737 - Introduce a new step for cleanup tasks" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#149 - Checkout of the default branch fails" $rlRun_LOG
@@ -26,7 +29,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Created (org:teemtee,packit)"
-        rlRun -s "did --config ./config-more.ini --gh-issues-created $YEAR_2023"
+        rlRun -s "$did ./config-more.ini --gh-issues-created $YEAR_2023"
         rlAssertGrep "Issues created on gh: 33$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#2493 - Implement retry functionality" $rlRun_LOG
         rlAssertGrep "packit/packit#1989 - Mention branch name" $rlRun_LOG
@@ -34,7 +37,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Created (repo:teemtee/fmf)"
-        rlRun -s "did --config ./config-repo.ini --gh-issues-created $YEAR_2022"
+        rlRun -s "$did ./config-repo.ini --gh-issues-created $YEAR_2022"
         rlAssertGrep "Issues created on gh: 2$" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#1737 - Introduce a new step for cleanup tasks" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#149 - Checkout of the default branch fails" $rlRun_LOG
@@ -42,17 +45,25 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Created (user:psss)"
-        rlRun -s "did --config ./config-user.ini --gh-issues-created $YEAR_2021"
+        rlRun -s "$did ./config-user.ini --gh-issues-created $YEAR_2021"
         rlAssertGrep "Issues created on gh: 1$" $rlRun_LOG
         rlAssertGrep "psss/did#247 - Implement pagination for the GitHub plugin" $rlRun_LOG
         rlAssertNotGrep "packit/packit#1386 - Allow to disable web access" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#910 - Shall we introduce a uuid for tests?" $rlRun_LOG
     rlPhaseEnd
 
+    rlPhaseStartTest "Issues Created, check correct --until"
+        rlRun -s "$did ./config-default.ini --gh-issues-created $CHECK_UNTIL"
+        rlAssertGrep "Issues created on gh: 1$" $rlRun_LOG
+        rlAssertGrep "teemtee/tmt#1559 - Update the overview of essential classes$" $rlRun_LOG
+        rlAssertNotGrep 'teemtee/tmt#1648' $rlRun_LOG
+        rlAssertNotGrep 'teemtee/tmt#1650' $rlRun_LOG
+    rlPhaseEnd
+
     # Issues Closed
 
     rlPhaseStartTest "Issues Closed"
-        rlRun -s "did --config ./config-default.ini --gh-issues-closed $YEAR_2022"
+        rlRun -s "$did ./config-default.ini --gh-issues-closed $YEAR_2022"
         rlAssertGrep "Issues closed on gh: 17$" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#014 - Define a way how to undefine an attribute" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#991 - Incompatible environment variable name" $rlRun_LOG
@@ -60,7 +71,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Closed (org:teemtee)"
-        rlRun -s "did --config ./config-org.ini --gh-issues-closed $YEAR_2022"
+        rlRun -s "$did ./config-org.ini --gh-issues-closed $YEAR_2022"
         rlAssertGrep "Issues closed on gh: 15$" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#014 - Define a way how to undefine an attribute" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#991 - Incompatible environment variable name" $rlRun_LOG
@@ -68,7 +79,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Closed (repo:teemtee/fmf)"
-        rlRun -s "did --config ./config-repo.ini --gh-issues-closed $YEAR_2022"
+        rlRun -s "$did ./config-repo.ini --gh-issues-closed $YEAR_2022"
         rlAssertGrep "Issues closed on gh: 3$" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#014 - Define a way how to undefine an attribute" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#991 - Incompatible environment variable name" $rlRun_LOG
@@ -76,7 +87,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Closed (user:psss)"
-        rlRun -s "did --config ./config-user.ini --gh-issues-closed $YEAR_2022"
+        rlRun -s "$did ./config-user.ini --gh-issues-closed $YEAR_2022"
         rlAssertGrep "Issues closed on gh: 1$" $rlRun_LOG
         rlAssertNotGrep "teemtee/fmf#014 - Define a way how to undefine an attribute" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#991 - Incompatible environment variable name" $rlRun_LOG

tests/github/pulls.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
+did="did --config"
+
 YEAR_2022="--since 2022-01-01 --until 2022-12-31"
 YEAR_2021="--since 2021-01-01 --until 2021-12-31"
+CHECK_UNTIL="--since 2022-10-01 --until 2022-10-26"
 
 rlJournalStart
 
     # Pull Requests Created
 
     rlPhaseStartTest "Pull Requests Created"
-        rlRun -s "did --config ./config-default.ini --gh-pull-requests-created $YEAR_2022"
+        rlRun -s "$did ./config-default.ini --gh-pull-requests-created $YEAR_2022"
         rlAssertGrep "Pull requests created on gh: 94$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#1750 - Include the new web link in verbose" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#170 - Implement a directive for disabling inheritance" $rlRun_LOG
@@ -20,7 +23,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Pull Requests Created (org:teemtee)"
-        rlRun -s "did --config ./config-org.ini --gh-pull-requests-created $YEAR_2022"
+        rlRun -s "$did ./config-org.ini --gh-pull-requests-created $YEAR_2022"
         rlAssertGrep "Pull requests created on gh: 85$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#1750 - Include the new web link in verbose" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#170 - Implement a directive for disabling inheritance" $rlRun_LOG
@@ -31,7 +34,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Pull Requests Created (org:teemtee,packit)"
-        rlRun -s "did --config ./config-more.ini --gh-pull-requests-created $YEAR_2022"
+        rlRun -s "$did ./config-more.ini --gh-pull-requests-created $YEAR_2022"
         rlAssertGrep "Pull requests created on gh: 86$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#1750 - Include the new web link in verbose" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#170 - Implement a directive for disabling inheritance" $rlRun_LOG
@@ -42,7 +45,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Pull Requests Created (repo:teemtee/fmf)"
-        rlRun -s "did --config ./config-repo.ini --gh-pull-requests-created $YEAR_2022"
+        rlRun -s "$did ./config-repo.ini --gh-pull-requests-created $YEAR_2022"
         rlAssertGrep "Pull requests created on gh: 7$" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#1750 - Include the new web link in verbose" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#170 - Implement a directive for disabling inheritance" $rlRun_LOG
@@ -53,7 +56,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Pull Requests Created (user:psss)"
-        rlRun -s "did --config ./config-user.ini --gh-pull-requests-created $YEAR_2022"
+        rlRun -s "$did ./config-user.ini --gh-pull-requests-created $YEAR_2022"
         rlAssertGrep "Pull requests created on gh: 7$" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#1750 - Include the new web link in verbose" $rlRun_LOG
         rlAssertNotGrep "teemtee/fmf#170 - Implement a directive for disabling inheritance" $rlRun_LOG
@@ -63,10 +66,18 @@ rlJournalStart
         rlAssertNotGrep "packit/packit.dev#399 - Update \`tmt\` examples" $rlRun_LOG
     rlPhaseEnd
 
+    rlPhaseStartTest "Pull Requests Created, check correct --until"
+        rlRun -s "$did ./config-default.ini --gh-pull-requests-created $CHECK_UNTIL"
+        rlAssertGrep "Pull requests created on gh: 7$" $rlRun_LOG
+        rlAssertGrep 'teemtee/tmt#1642 - Move the hardware specification into a separate page$' $rlRun_LOG
+        rlAssertNotGrep 'teemtee/tmt#1645' $rlRun_LOG
+        rlAssertNotGrep 'teemtee/tmt#1644' $rlRun_LOG
+    rlPhaseEnd
+
     # Pull Requests Closed
 
     rlPhaseStartTest "Pull Requests Closed"
-        rlRun -s "did --config ./config-default.ini --gh-pull-requests-closed $YEAR_2022"
+        rlRun -s "$did ./config-default.ini --gh-pull-requests-closed $YEAR_2022"
         rlAssertGrep "Pull requests closed on gh: 315$" $rlRun_LOG
         rlAssertGrep "psss/did#272 - Koji plugin" $rlRun_LOG
         rlAssertGrep "psss/python-nitrate#038 - Properly handle string" $rlRun_LOG
@@ -76,7 +87,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Pull Requests Closed (org:teemtee)"
-        rlRun -s "did --config ./config-org.ini --gh-pull-requests-closed $YEAR_2022"
+        rlRun -s "$did ./config-org.ini --gh-pull-requests-closed $YEAR_2022"
         rlAssertGrep "Pull requests closed on gh: 305$" $rlRun_LOG
         rlAssertNotGrep "psss/did#272 - Koji plugin" $rlRun_LOG
         rlAssertNotGrep "psss/python-nitrate#038 - Properly handle string" $rlRun_LOG
@@ -86,7 +97,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Pull Requests Closed (repo:teemtee/fmf)"
-        rlRun -s "did --config ./config-repo.ini --gh-pull-requests-closed $YEAR_2022"
+        rlRun -s "$did ./config-repo.ini --gh-pull-requests-closed $YEAR_2022"
         rlAssertGrep "Pull requests closed on gh: 13$" $rlRun_LOG
         rlAssertNotGrep "psss/did#272 - Koji plugin" $rlRun_LOG
         rlAssertNotGrep "psss/python-nitrate#038 - Properly handle string" $rlRun_LOG
@@ -96,7 +107,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Pull Requests Closed (user:psss)"
-        rlRun -s "did --config ./config-user.ini --gh-pull-requests-closed $YEAR_2022"
+        rlRun -s "$did ./config-user.ini --gh-pull-requests-closed $YEAR_2022"
         rlAssertGrep "Pull requests closed on gh: 10$" $rlRun_LOG
         rlAssertGrep "psss/did#272 - Koji plugin" $rlRun_LOG
         rlAssertGrep "psss/python-nitrate#038 - Properly handle string" $rlRun_LOG

commit 035ce629e11ecfde1b98d0a9c3533fb0889dcae2

Use the correct class for `until` in the `github` plugin
Fix regression introduced in #376. The `until` date should use the
`did.base.Date` class. Otherwise stats like commented issues
traceback because of the missing `datetime` attribute. Include a
basic sanity coverage which exercises all stats to prevent similar
issues in the future.

did/plugins/github.py
@@ -52,7 +52,7 @@ import requests
 from tenacity import (RetryError, Retrying, retry_if_exception_type,
                       stop_after_attempt)
 
-from did.base import Config, ReportError, get_token
+from did.base import Config, Date, ReportError, get_token
 from did.stats import Stats, StatsGroup
 from did.utils import listed, log, pretty
 
@@ -126,7 +126,7 @@ class GitHub():
     @staticmethod
     def until(until):
         """Issue #362: until for GH should have - delta(day=1)"""
-        return until - 1
+        return Date(until - 1)
 
     def request(self, url):
         while True:

tests/github/main.fmf
@@ -2,6 +2,12 @@
     summary: Check the help message and option sanity
     test: ./help.sh
 
+/basic:
+    summary: Check stats for the last week
+    description: Ensures that all stats are sane
+    framework: shell
+    test: did --config ./config-default.ini
+
 /pulls:
     summary: Verify pull request stats
     test: ./pulls.sh