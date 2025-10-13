bb13a2a0e6aa8744ebd34087ed999c015a9ae96c
Sandro Bonazzola <sbonazzo@redhat.com>
2025-10-13 11:27:03 +0200

    Add typing hints to conftest.py (#430)
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
+from _pytest.config.argparsing import Parser
+from _pytest.nodes import Item
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: Parser) -> None:
     parser.addoption("--functional", action="store_true",
                      help="run the functional tests (marked with marker @functional)")
 
 
-def pytest_runtest_setup(item):
+def pytest_runtest_setup(item: Item) -> None:
     if 'functional' in item.keywords and not item.config.getoption("--functional"):
         pytest.skip("need --functional option to run this test")
