60e0beda49a6c8af9baf6533560338a1b9f020ac
Petr Šplíchal <psplicha@redhat.com>
2026-03-30 12:36:20 +0200

    Just disable unit test on centos-stream
    The collect ignore does not work with older pytest:
    
    ```
      File "/usr/lib/python3.9/site-packages/pluggy/manager.py", line 252, in _verify_hook
        raise PluginValidationError(
    pluggy.manager.PluginValidationError: Plugin '/var/ARTIFACTS/work-basiciy0u5a0r/plans/basic/discover/default-0/tests/conftest.py' for hook 'pytest_ignore_collect'
    hookimpl definition: pytest_ignore_collect(collection_path, config) -> bool
    Argument(s) {'collection_path'} are declared in the hookimpl but can not be found in the hookspec
    ```
    
    Let's just skip centos stream for now.

--- conftest.py
+++ conftest.py
@@ -4,21 +4,8 @@ from _pytest.nodes import Item
 
 
 def pytest_addoption(parser: Parser) -> None:
-    parser.addoption(
-        "--functional",
-        action="store_true",
-        help="run the functional tests (marked with marker @functional)")
-
-    parser.addoption(
-        "--no-google",
-        action="store_true",
-        help="disable the google tests (require the google module)")
-
-
-def pytest_ignore_collect(collection_path, config) -> bool:
-    if collection_path.name == "test_google.py" and config.getoption("--no-google"):
-        return True
-    return False
+    parser.addoption("--functional", action="store_true",
+                     help="run the functional tests (marked with marker @functional)")
 
 
 def pytest_runtest_setup(item: Item) -> None:
