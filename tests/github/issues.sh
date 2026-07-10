f3e3ba925e413d775348e8fa431e228f39979a6c
Petr Šplíchal <psplicha@redhat.com>
2026-07-10 16:43:12 +0200

    Modify github test data (#473)
    Seems that the issue search query does not return all items as expected.
    Adjust test data accordingly. Filed a support ticket here:
    
    * https://support.github.com/ticket/personal/0/4551935

--- tests/github/issues.sh
+++ tests/github/issues.sh
@@ -14,17 +14,17 @@ rlJournalStart
 
     rlPhaseStartTest "Issues Created"
         rlRun -s "$did ./config-default.ini --gh-issues-created $YEAR_2022"
-        rlAssertGrep "Issues created on gh: 31$" $rlRun_LOG
+        rlAssertGrep "Issues created on gh: 30$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#1737 - Introduce a new step for cleanup tasks" $rlRun_LOG
-        rlAssertGrep "teemtee/fmf#149 - Checkout of the default branch fails" $rlRun_LOG
+        rlAssertGrep "teemtee/fmf#169 - Use \`--depth 1\` when cloning git repos" $rlRun_LOG
         rlAssertGrep "packit/packit-service#1645 - Manually trigger internal jobs" $rlRun_LOG
     rlPhaseEnd
 
     rlPhaseStartTest "Issues Created (org:teemtee)"
         rlRun -s "$did ./config-org.ini --gh-issues-created $YEAR_2022"
-        rlAssertGrep "Issues created on gh: 30$" $rlRun_LOG
+        rlAssertGrep "Issues created on gh: 29$" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#1737 - Introduce a new step for cleanup tasks" $rlRun_LOG
-        rlAssertGrep "teemtee/fmf#149 - Checkout of the default branch fails" $rlRun_LOG
+        rlAssertGrep "teemtee/fmf#169 - Use \`--depth 1\` when cloning git repos" $rlRun_LOG
         rlAssertNotGrep "packit/packit-service#1645 - Manually trigger internal jobs" $rlRun_LOG
     rlPhaseEnd
 
@@ -38,9 +38,9 @@ rlJournalStart
 
     rlPhaseStartTest "Issues Created (repo:teemtee/fmf)"
         rlRun -s "$did ./config-repo.ini --gh-issues-created $YEAR_2022"
-        rlAssertGrep "Issues created on gh: 2$" $rlRun_LOG
+        rlAssertGrep "Issues created on gh: 1$" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#1737 - Introduce a new step for cleanup tasks" $rlRun_LOG
-        rlAssertGrep "teemtee/fmf#149 - Checkout of the default branch fails" $rlRun_LOG
+        rlAssertGrep "teemtee/fmf#169 - Use \`--depth 1\` when cloning git repos" $rlRun_LOG
         rlAssertNotGrep "packit/packit-service#1645 - Manually trigger internal jobs" $rlRun_LOG
     rlPhaseEnd
 
@@ -64,7 +64,7 @@ rlJournalStart
 
     rlPhaseStartTest "Issues Closed"
         rlRun -s "$did ./config-default.ini --gh-issues-closed $YEAR_2022"
-        rlAssertGrep "Issues closed on gh: 17$" $rlRun_LOG
+        rlAssertGrep "Issues closed on gh: 16$" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#014 - Define a way how to undefine an attribute" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#991 - Incompatible environment variable name" $rlRun_LOG
         rlAssertGrep "psss/did#269 - Invalid plugin type 'google'" $rlRun_LOG
@@ -72,7 +72,7 @@ rlJournalStart
 
     rlPhaseStartTest "Issues Closed (org:teemtee)"
         rlRun -s "$did ./config-org.ini --gh-issues-closed $YEAR_2022"
-        rlAssertGrep "Issues closed on gh: 15$" $rlRun_LOG
+        rlAssertGrep "Issues closed on gh: 14$" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#014 - Define a way how to undefine an attribute" $rlRun_LOG
         rlAssertGrep "teemtee/tmt#991 - Incompatible environment variable name" $rlRun_LOG
         rlAssertNotGrep "psss/did#269 - Invalid plugin type 'google'" $rlRun_LOG
@@ -80,7 +80,7 @@ rlJournalStart
 
     rlPhaseStartTest "Issues Closed (repo:teemtee/fmf)"
         rlRun -s "$did ./config-repo.ini --gh-issues-closed $YEAR_2022"
-        rlAssertGrep "Issues closed on gh: 3$" $rlRun_LOG
+        rlAssertGrep "Issues closed on gh: 2$" $rlRun_LOG
         rlAssertGrep "teemtee/fmf#014 - Define a way how to undefine an attribute" $rlRun_LOG
         rlAssertNotGrep "teemtee/tmt#991 - Incompatible environment variable name" $rlRun_LOG
         rlAssertNotGrep "psss/did#269 - Invalid plugin type 'google'" $rlRun_LOG
